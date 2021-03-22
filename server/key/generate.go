8ce2960fb7c00479140075a60a7dceab97ca5a64
isso0424 <isso04240129@gmail.com>
2021-03-23 03:37:08 +0900

    fix

--- server/key/generate.go
+++ server/key/generate.go
@@ -27,7 +27,7 @@ func Generate() string {
 		return ""
 	}
 
-	request, err := http.NewRequest("GET", "https://api.github.com/app/installations/1/access_tokens", nil)
+	request, err := http.NewRequest("GET", "https://api.github.com/app/installations", nil)
 	if err != nil {
 		log.Println(err)
 		return ""
