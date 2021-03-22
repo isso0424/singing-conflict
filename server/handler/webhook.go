72edc8a3ca4f2acd0fe172f401f0ecb4c2326142
isso0424 <isso04240129@gmail.com>
2021-03-23 03:40:18 +0900

    test

--- server/handler/webhook.go
+++ server/handler/webhook.go
@@ -22,7 +22,6 @@ func signBody(secret, body []byte) []byte {
 	return []byte(computed.Sum(nil))
 }
 
-
 func verifySignature(secret []byte, signature string, body []byte) bool {
 	const signaturePrefix = "sha1="
 	const signatureLength = 45 // len(SignaturePrefix) + len(hex(sha1))
